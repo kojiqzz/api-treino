39212487615053aea9bbe3cfa1d831c6e768908f
kojiqzz <victorhugosantos360@gmail.com>
2024-08-02 18:12:46 -0300

    Codigo subtrção feito

--- api.js
+++ api.js
@@ -10,28 +10,34 @@ servidor.get('/calculadora/soma/:n1/:n2', (req, resp) => {
 
     let n1= Number(req.params.n1);
     let n2= Number(req.params.n2);
-
     let calculo = n1 + n2
 
    
-
     resp.send({
-
-            entradas:{
-                 
+            entradas:{ 
                 entrada1: n1,
                 entrada2: n2
             },
-        
         soma : calculo
 
-    })
-
-     
+    })   
 })
 
 
+servidor.get('/calculadora/subtrair/:n1/:n2', (req, resp) => {
+    let n1=Number(req.params.n1)
+    let n2=Number(req.params.n2)
 
+    let calculo= n1 - n2
+
+    resp.send({
+            entradas:{
+                entrada1: n1,
+                entrada2: n2
+            },
+            subtracao: calculo
+    })
+})
 
 servidor.listen(2008, () => console.log("api subiu na porta 2008"))
 
