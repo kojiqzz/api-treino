d0d51f4bf844b0283bdd43a2266eb953fe2150eb
voltareli <nivoltareli@gmail.com>
2024-08-02 18:02:11 -0300

    da

---
+++ api.js
@@ -0,0 +1,40 @@
+import express from 'express' 
+const servidor = express() ;
+
+
+
+
+
+
+servidor.get('/calculadora/soma/:n1/:n2', (req, resp) => {
+
+    let n1= Number(req.params.n1);
+    let n2= Number(req.params.n2);
+
+    let calculo = n1 + n2
+
+   
+
+    resp.send({
+
+            entradas:{
+                 
+                entrada1: n1,
+                entrada2: n2
+            },
+        
+        soma : calculo
+
+    })
+
+     
+})
+
+
+
+
+servidor.listen(2008, () => console.log("api subiu na porta 2008"))
+
+
+
+;
