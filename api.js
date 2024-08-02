e7319668adf751cd63b67e4cc52289d0cc623437
kojiqzz <victorhugosantos360@gmail.com>
2024-08-02 18:17:44 -0300

    TA feito

--- api.js
+++ api.js
@@ -23,7 +23,7 @@ servidor.get('/calculadora/soma/:n1/:n2', (req, resp) => {
     })   
 })
 
-
+// ola
 servidor.get('/calculadora/subtrair/:n1/:n2', (req, resp) => {
     let n1=Number(req.params.n1)
     let n2=Number(req.params.n2)
